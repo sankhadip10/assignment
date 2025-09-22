1278824090235278e0ea3eb6d8d32341ac42444c
Sankhadip Das <121531893+sankhadip10@users.noreply.github.com>
2025-09-22 07:26:14 +0530

    Remove unused Jest configuration options

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -4,17 +4,4 @@ module.exports = {
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
   },
-  transform: {
-  '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
-  },
-  testPathIgnorePatterns: [
-    '<rootDir>/.next/',
-    '<rootDir>/node_modules/',
-    '<rootDir>/cypress/'
-  ],
-  collectCoverageFrom: [
-    'src/**/*.{js,jsx,ts,tsx}',
-    '!src/**/*.d.ts',
-    '!src/**/*.stories.{js,jsx,ts,tsx}',
-  ],
 };
